428f9b61227f6e7318fdb2ac9b52d60971c19e75
roboticprocessauto <166108544+roboticprocessauto@users.noreply.github.com>
2025-07-04 14:39:27 +0300

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// Firebase Configuration
+// Это клиентская конфигурация - эти данные не секретные и предназначены для публичного использования
+window.firebaseConfig = {
+    apiKey: "AIzaSyA7a22ZA0sjtPKof0GmwcAnNmHZ4s7d7U4",
+    authDomain: "checker-7f7b7.firebaseapp.com",
+    databaseURL: "https://checker-7f7b7-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "checker-7f7b7",
+    storageBucket: "checker-7f7b7.firebasestorage.app",
+    messagingSenderId: "55724792345",
+    appId: "1:55724792345:web:5df424f16e495d8f38a888",
+    measurementId: "G-2F94LPCYYM"
+};
