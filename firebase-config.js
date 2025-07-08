eab1d0f6a7a826e70b3ccfc63345236fd8f6f381
roboticprocessauto <166108544+roboticprocessauto@users.noreply.github.com>
2025-07-08 11:05:19 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,14 +1,16 @@
 // ===== FIREBASE CONFIGURATION =====
 
-// Firebase конфигурация
+// Firebase Configuration
+// Это клиентская конфигурация - эти данные не секретные и предназначены для публичного использования
 window.firebaseConfig = {
-    apiKey: "AIzaSyDH8ZOW4fU5KfUGv-QeR8A1HGl-VJRlZJE",
-    authDomain: "maxbet-demo.firebaseapp.com",
-    databaseURL: "https://maxbet-demo-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "maxbet-demo",
-    storageBucket: "maxbet-demo.appspot.com",
-    messagingSenderId: "123456789012",
-    appId: "1:123456789012:web:abcdef123456789012345678"
+    apiKey: "AIzaSyA7a22ZA0sjtPKof0GmwcAnNmHZ4s7d7U4",
+    authDomain: "checker-7f7b7.firebaseapp.com",
+    databaseURL: "https://checker-7f7b7-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "checker-7f7b7",
+    storageBucket: "checker-7f7b7.firebasestorage.app",
+    messagingSenderId: "55724792345",
+    appId: "1:55724792345:web:5df424f16e495d8f38a888",
+    measurementId: "G-2F94LPCYYM"
 };
 
 // Проверка конфигурации
